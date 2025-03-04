cfd693d7b86521752b490d371318ad7bb9f3c917
Taverlon <nitronolay@gmail.com>
2025-03-04 17:08:11 +0500

    готовый проект

---
+++ js/script.js
@@ -0,0 +1,45 @@
+// подгрузка данных в таблицу
+Orders.forEach(order => {
+    const tr = document.createElement('tr');
+    const trContent = 
+    `
+    <td data-label="Товар">${order.productName}</td>
+    <td data-label="Артикул">${order.productNumber}</td>
+    <td data-label="Статус-заказа">${order.orderStatus}</td>
+    <td data-label="Статус-оплаты" class="${
+        order.paymentStatus === 'Не оплачен' ? 'danger' : 
+        order.paymentStatus === 'В обработке' ? 'warning' : 
+        'success'}">${order.paymentStatus}</td>
+    <td class="primary" >Подробнее</td>
+    `;
+
+    tr.innerHTML = trContent;
+    document.querySelector('table tbody').appendChild(tr);
+}
+)
+
+// открытие меню
+
+const sideMenu = document.querySelector('.left');
+
+const menuBtn = document.querySelector('#menu_btn')
+
+const closeBtn = document.querySelector('#close-btn')
+
+menuBtn.addEventListener('click', () => {
+    sideMenu.style.display = 'block';
+})
+
+closeBtn.addEventListener('click', () =>{
+    sideMenu.style.display = 'none'
+})
+
+// переключение темы
+
+const themeToggler = document.querySelector('.theme-toggler');
+
+themeToggler.addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme-variables');
+    themeToggler.querySelector('span:nth-child(1)').classList.toggle('on')
+    themeToggler.querySelector('span:nth-child(1)').classList.toggle('on')
+})
